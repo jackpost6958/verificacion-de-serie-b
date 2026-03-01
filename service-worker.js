ac55a9aad39b9210371547ed6a878c5e97ee05af
jackpost6958 <69641425+jackpost6958@users.noreply.github.com>
2026-03-01 14:53:51 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,7 @@
-const CACHE_NAME = 'validador-billetes-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json'
-];
-
-// Instalar y cachear recursos
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+const CACHE = "v1";
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./style.css", "./script.js", "./manifest.json"])));
 });
-
-// Responder desde el cache
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
 });
